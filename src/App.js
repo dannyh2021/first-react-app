58653af8bce714ae0be9de14ad1ef5da7d499685
dannyh2021 <32226840+dannyh2021@users.noreply.github.com>
2021-12-31 15:17:43 -0500

    Added features
    Added search function. Also the app component fetches and displays a list from an API.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,139 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DEFAULT_QUERY = 'redux'
+
+const PATH_BASE = 'https://hn.algolia.com/api/v1'
+const PATH_SEARCH = '/search'
+const PARAM_SEARCH = 'query='
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`
+
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase())
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY
+    }
+
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.onSearchChange = this.onSearchChange.bind(this)
+    this.onDismiss = this.onDismiss.bind(this)
+  }
+
+  //need to update
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id
+    const updatedList = this.state.result.filter(isNotId)
+    this.setState({ result: updatedList })
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result })
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error)
+  }
+
+  render() {
+    console.log(this.state)
+    const { searchTerm, result } = this.state
+
+    if (!result) {
+      return null;
+    }
+
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          >
+            Search
+          </Search>
+        </div>
+        <Table
+          list={result.hits}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    )
+  }
+}
+
+const Search = ({ value, onChange, children }) => 
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props
+    return (
+      <div className="table">
+        {list.filter(isSearched(pattern)).map(item => 
+          <div key={item.objectID} className="table-row">
+            <span>
+              <a href = {item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <Button
+                onClick={() => onDismiss(item.objectID)}
+                className="button-inline"
+              >
+                Dismiss
+              </Button>
+            </span>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className = '',
+      children
+    } = this.props
+
+    return (
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+    )
+  }
 }
 
 export default App;
