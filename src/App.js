f7065f4d33ff2e8c800d2d0fc6c9cd751ca512ae
dannyh2021 <32226840+dannyh2021@users.noreply.github.com>
2022-01-01 20:28:50 -0500

    added sorting
    includes sorting by different attributes, reverse sorting, and changed style for the active sort.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { sortBy } from 'lodash'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,8 +17,13 @@ const PARAM_HPP = 'hitsPerPage='
 
 const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`
 
-const isSearched = searchTerm => item =>
-  item.title.toLowerCase().includes(searchTerm.toLowerCase())
+const SORTS = {
+  NONE: list => list,
+  TITLE: list => sortBy(list, 'title'),
+  AUTHOR: list => sortBy(list, 'num_comments').reverse(),
+  COMMENTS: list => sortBy(list, 'num_comments').reverse(),
+  POINTS: list => sortBy(list, 'points').reverse()
+}
 
 class App extends Component {
   constructor(props) {
@@ -27,7 +34,9 @@ class App extends Component {
       searchKey: '',
       searchTerm: DEFAULT_QUERY,
       error: null,
-      isLoading: false
+      isLoading: false,
+      sortKey: 'NONE',
+      isSortReverse: false
     }
 
     this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
@@ -36,6 +45,7 @@ class App extends Component {
     this.onSearchChange = this.onSearchChange.bind(this)
     this.onSearchSubmit = this.onSearchSubmit.bind(this)
     this.onDismiss = this.onDismiss.bind(this)
+    this.onSort = this.onSort.bind(this)
   }
 
   onDismiss(id) {
@@ -101,6 +111,11 @@ class App extends Component {
       .catch(error => this.setState({ error }))
   }
 
+  onSort(sortKey) {
+    const isSortReverse = this.state.sortKey === sortKey && !this.state.isSortReverse
+    this.setState({ sortKey, isSortReverse })
+  }
+
   componentDidMount() {
     const { searchTerm } = this.state
     this.setState({ searchKey: searchTerm })
@@ -113,7 +128,9 @@ class App extends Component {
       results,
       searchKey,
       error,
-      isLoading
+      isLoading,
+      sortKey,
+      isSortReverse
     } = this.state
 
     const page = (
@@ -145,6 +162,9 @@ class App extends Component {
         </div>
         <Table
           list={list}
+          sortKey={sortKey}
+          isSortReverse={isSortReverse}
+          onSort={this.onSort}
           onDismiss={this.onDismiss}
         />
         <div className="interactions">
@@ -195,10 +215,56 @@ class Search extends Component {
 
 class Table extends Component {
   render() {
-    const { list, onDismiss } = this.props
+    const { list, onDismiss, isSortReverse, onSort, sortKey } = this.props
+    const sortedList = SORTS[sortKey](list)
+    const reverseSortedList = isSortReverse
+      ? sortedList.reverse()
+      : sortedList
+
     return (
       <div className="table">
-        {list.map(item => 
+        <div className="table-header">
+          <span style={{ width: '40%' }}>
+            <Sort
+              sortKey={'TITLE'}
+              onSort={onSort}
+              activeSortKey={sortKey}
+            >
+              Title
+            </Sort>
+          </span>
+          <span style={{ width: '30%' }}>
+            <Sort
+              sortKey={'AUTHOR'}
+              onSort={onSort}
+              activeSortKey={sortKey}
+            >
+              Author
+            </Sort>
+          </span>
+          <span style={{ width: '10%' }}>
+            <Sort
+              sortKey={'COMMENTS'}
+              onSort={onSort}
+              activeSortKey={sortKey}
+            >
+              Comments
+            </Sort>
+          </span>
+          <span style={{ width: '10%' }}>
+            <Sort
+              sortKey={'POINTS'}
+              onSort={onSort}
+              activeSortKey={sortKey}
+            >
+              Points
+            </Sort>
+          </span>
+          <span style={{ width: '10%' }}>
+            Archive
+          </span>
+        </div>
+        {reverseSortedList.map(item => 
           <div key={item.objectID} className="table-row">
             <span>
               <a href = {item.url}>{item.title}</a>
@@ -219,6 +285,26 @@ class Table extends Component {
       </div>
     )
   }
+}
+
+const Sort = ({ sortKey, activeSortKey, onSort, children }) =>{
+  const sortClass = classNames(
+    'button-inline',
+    { 'button-active': sort === activeSortKey }
+  )
+
+  if(sortKey === activeSortKey) {
+    sortClass.push('button-active')
+  }
+
+  return(
+    <Button
+    onClick={() => onSort(sortKey)}
+    className={sortClass}
+    >
+      {children}
+    </Button>
+  )
 }
 
 class Button extends Component {
